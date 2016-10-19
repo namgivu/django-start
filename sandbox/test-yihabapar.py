24c42c54a9b008bd350d18034754ffce93fa4ddb
namgivu <namgivu@gmail.com>
2016-10-19 17:43:25 +0700

    update test yihabb

--- sandbox/test-yihabapar.py
+++ sandbox/test-yihabapar.py
@@ -17,3 +17,16 @@ run_bash('{APP_HOME}/sandbox/test-get_arg.py {arg01} {arg02}'.format(
   arg01=122,
   arg02='abb')
 )
+
+run_bash('{APP_HOME}/sandbox/test-ext_clone.py {url} {branch} {key} {CLONED_TO}'.format(
+  APP_HOME=APP_HOME,
+  url='git@github.com:hoangphuong/personal_robot_abilities.git',
+  branch='master',
+  key='/home/ubuntu/.ssh/namgivu-github-ssh',
+  CLONED_TO='ext/hoangphuong.maya')
+)
+
+run_bash('{APP_HOME}/sandbox/test-ext_remove.py {REMOVED_EXT}'.format(
+  APP_HOME=APP_HOME,
+  REMOVED_EXT='ext/hoangphuong.maya')
+)
