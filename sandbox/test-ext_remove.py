63c722c4548c56f0be22547ba52bc1d770b8cfcd
namgivu <namgivu@gmail.com>
2016-10-19 17:43:50 +0700

    test git submodule clone

---
+++ sandbox/test-ext_remove.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+#region import yihabapar utility
+import sys, os
+UTIL_HOME='{APP_HOME}/util'.format(APP_HOME='%s/..' % os.path.abspath(os.path.dirname(__file__)) )
+sys.path.insert(0, UTIL_HOME)
+from util_init import *
+#endregion import yihabapar utility
+
+ext_remove(REMOVED_EXT = 'ext/hoangphuong.maya')
