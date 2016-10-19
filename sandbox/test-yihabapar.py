34efb47f64ddd5916dd99bd1df148e5d69bcffe4
namgivu <namgivu@gmail.com>
2016-10-19 11:17:35 +0700

    yihabapar test

---
+++ sandbox/test-yihabapar.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+'''
+test util from yihabapar project ref. https://github.com/namgivu/yihabapar
+'''
+
+#region import yihabapar utility
+import sys, os
+UTIL_HOME='{APP_HOME}/util'.format(APP_HOME='%s/..' % os.path.abspath(os.path.dirname(__file__)) )
+sys.path.insert(0, UTIL_HOME)
+from util_init import *
+#endregion import yihabapar utility
+
+run_bash('echo yihabapar')
