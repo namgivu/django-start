fba2776eadfb6764be268e8f684062b20f147769
namgivu <namgivu@gmail.com>
2017-07-31 20:19:03 +0700

    /polls @ define view handler

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+  from django.http import HttpResponse
+  return HttpResponse("Hello, world. You're at the polls index.")
