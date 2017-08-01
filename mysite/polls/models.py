cd587ea3cd31246a9553a577a6bee13167a63d78
namgivu <namgivu@gmail.com>
2017-08-01 12:51:36 +0700

    add models

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,6 +1,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+  question_text = models.CharField(max_length=200)
+  pub_date      = models.DateTimeField('date published') #an optional first-positional-argument to giv a human-readable name
+
+
+class Choice(models.Model):
+  choice_text = models.CharField(max_length=200)
+  votes       = models.IntegerField(default=0)
+
+  question = models.ForeignKey(Question, on_delete=models.CASCADE)
