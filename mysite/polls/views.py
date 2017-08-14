2a68c9e1fc9367c493a84ceccd493aa45c936a1d
namgivu <namgivu@gmail.com>
2017-08-14 11:47:18 +0700

    more view with param

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,21 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 def index(request):
-  from django.http import HttpResponse
-  return HttpResponse("Hello, world. You're at the polls index.")
+  return HttpResponse('This is polls index.')
+
+
+def detail(request, question_id):
+  return HttpResponse('Detail of question %s.' % question_id)
+
+
+def results(request, question_id):
+  return HttpResponse('Result of question %s.' % question_id)
+
+
+def vote(request, question_id):
+  return HttpResponse('Voting on question %s.' % question_id)
+
