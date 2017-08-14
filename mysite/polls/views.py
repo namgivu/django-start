294ad4e66f9def16a3b8903af35d5769f19a270a
namgivu <namgivu@gmail.com>
2017-08-14 12:55:27 +0700

    get model by id w/ error handler

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 from models import Question
+from django.http import Http404
 
 
 def index(request):
@@ -21,6 +22,14 @@ def index(request):
 
 def detail(request, question_id):
   return HttpResponse('Detail of question %s.' % question_id)
+
+
+def detail(request, question_id):
+  try:
+    question = Question.objects.get(pk=question_id)
+  except Question.DoesNotExist:
+    raise Http404("Question does not exist")
+  return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
